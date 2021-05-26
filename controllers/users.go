896613f7154000717251858408532cc928fcb055
Shashaank Babu <shashaankramesh@gmail.com>
2021-05-26 16:42:12 +0530

    Added Create() method to the user controller

--- controllers/users.go
+++ controllers/users.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"lenslocked/views"
 	"net/http"
 )
@@ -18,8 +19,16 @@ type Users struct {
 	NewView *views.View
 }
 
+// New() is used to render a form for account creation.
+// GET /signup
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 	if err := u.NewView.Render(w, nil); err != nil {
 		panic(err)
 	}
+}
+
+// Create() is used to process a signup form.
+// POST /signup
+func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "This is temp response.")
 }
